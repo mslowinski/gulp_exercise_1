b46941fc98fd62c5bf57c776d43ed10dbc35c10b
Marcin Slowinski <marcin@MacBook-Pro-Marcin.local>
2015-11-25 00:04:26 +0100

    remove unused variables

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var gulp = require('gulp');
 var concatWrap = require('./gulp-concat-wrap/gulp-concat-wrap.js');
 
 
-var prefix = "//prefix code \n";
-var suffix = "\n//suffix code";
-
-
 gulp.task('concatWrap', function() {
    return gulp.src('src/*.js')
        .pipe(concatWrap({name: 'out.js', prefix: '//prefix', suffix: '//suffix'}))
